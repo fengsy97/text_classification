1f4220e7bb93d8692d68798074570648bbd79696
fengsy97 <42894515+fengsy97@users.noreply.github.com>
2019-12-20 17:00:01 +0800

    add data_handle.py

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# text_classification
+# text_classification  
+调参：
+Train with or without dropout. (The dropout rate should be set to 0.2)
+Train with different hidden size. (The hidden size should be set to 256
+and 512)
+Train with a different number of the hidden layer. (The number of hidden
+layer should be set to 1 and 3)
+
+Train with pre-trained word embedding. (We supply GloVe pretrained
+word embedding with 300-dimension for your experiments and you can
+explore the model performance with the same dimension without pretrained word embeddings.)
+
+  
+需要比较上述参数设置不同的精确度差异  
+可以考虑如何利用短语和单词的情感标签来改进模型
---
+++ src/data_handle.py
@@ -0,0 +1,30 @@
+#data_handle.py
+#(2 (3 (3 Effective) (2 but)) (1 (1 too-tepid) (2 biopic)))
+def data_handle(str_temp):
+    len_str = len(str_temp)
+    result = ""
+    while(str_temp[0] == "("):
+        end_rank = 0
+        while(end_rank < len(str_temp)):
+            if(str_temp[end_rank] == ")"):
+                break
+            end_rank += 1
+        start_rank = end_rank
+        while start_rank >= 0:
+            start_rank -= 1
+            if(str_temp[start_rank] == "("):
+                break
+        kongge_rank = start_rank
+        while kongge_rank < end_rank:
+            kongge_rank += 1
+            if(str_temp[kongge_rank] == " "):
+                break
+        # print(str_temp[kongge_rank+1:end_rank],str_temp[start_rank+1:kongge_rank])
+        str_temp = str_temp[:start_rank]+str_temp[kongge_rank+1:end_rank]+str_temp[end_rank+1:]
+    print(str_temp)
+
+input_path = "../data/trees/test.txt"
+f_input=open(input_path,'r',encoding = 'utf-8')
+lines = f_input.readlines()
+for line in lines:
+    data_handle(line.strip())
